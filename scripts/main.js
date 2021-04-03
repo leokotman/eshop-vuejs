036157910a08bc59103eb5f24dd9a933860562ab
levkotman <leoktmn@yandex.ru>
2021-04-03 14:54:47 +0200

    added items

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,36 @@
-const app = new Vue();
-console.log(app);
+new Vue({
+	el: '#app',
+
+	data() {
+		return {
+			goods: [
+				{title: "Shirt",price: 150},
+	            {title: "Socks",price: 50},
+	            {title: "Jacket",price: 350},
+	            {title: "Shoes",price: 250},
+	            {title: "Pyjamas",price: 100},
+	            {title: "Hat",price: 100},
+	            {title: "Umbrella",price: 120},
+	            {title: "Shorts",price: 130},
+	            {title: "Pants",price: 200},
+            ],
+			filteredGoods: [
+				{title: "Shirt",price: 150},
+	            {title: "Socks",price: 50},
+	            {title: "Jacket",price: 350},
+	            {title: "Shoes",price: 250},
+	            {title: "Pyjamas",price: 100},
+	            {title: "Hat",price: 100},
+	            {title: "Umbrella",price: 120},
+	            {title: "Shorts",price: 130},
+	            {title: "Pants",price: 200}
+            ],
+			searchLine: ''
+		}	
+	},
+
+	methods: {
+		
+	},
+
+});
